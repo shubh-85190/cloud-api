932fa56618f09b05b797b13769f7304c2f54946e
Shubham <shubh85190@gmail.com>
2023-09-26 20:04:43 +0530

    commit from office machine app start coding

--- app-ui/src/app/app.module.ts
+++ app-ui/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AddItemComponent } from './add-item/add-item.component';
+import { ViewCartComponent } from './view-cart/view-cart.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AddItemComponent,
+    ViewCartComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
